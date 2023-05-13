86b984c3125ee8a18582f2209dba8f15310fd462
Antonio Fiamenghi Neto <neto.fiamenghi@gmail.com>
2023-05-13 15:21:27 -0300

    Add files via upload

---
+++ src/main/java/com/api/parkingcontrol/repositores/ParkingSpotRepository.java
@@ -0,0 +1,15 @@
+package com.api.parkingcontrol.repositores;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+    
+    boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
+
+}
